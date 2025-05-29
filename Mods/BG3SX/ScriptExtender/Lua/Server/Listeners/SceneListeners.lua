cad1772dde2ee93421c31c451e38bd338d3376a6
RlySkiz <leinad92@hotmail.de>
2025-05-29 15:54:12 +0200

    Removed old spell logic
    Since we don't use spells anymore
    In case some characters avoid getting their spells removed they at least can't do anything with them

--- Mods/BG3SX/ScriptExtender/Lua/Server/Listeners/SceneListeners.lua
+++ Mods/BG3SX/ScriptExtender/Lua/Server/Listeners/SceneListeners.lua
@@ -3,35 +3,35 @@
 ------------------------------------------------------------------------------------------------------------------------------------------
 
 Ext.Osiris.RegisterListener("UsingSpellAtPosition", 8, "after", function(caster, x, y, z, spell, spellType, spellElement, storyActionID)
-    local location = {x = x, y = y, z = z}
-    if spell == "BG3SX_ChangeSceneLocation" then
-        Scene:MoveSceneToLocation(caster, location)
-    end
-    if spell == "BG3SX_RotateScene" then
-        Scene:RotateScene(caster, location)
-    end
+    -- local location = {x = x, y = y, z = z}
+    -- if spell == "BG3SX_ChangeSceneLocation" then
+    --     Scene:MoveSceneToLocation(caster, location)
+    -- end
+    -- if spell == "BG3SX_RotateScene" then
+    --     Scene:RotateScene(caster, location)
+    -- end
 end)
 
 Ext.Osiris.RegisterListener("UsingSpell", 5, "after", function(caster, spell, _, _, _)
-    if spell == "BG3SX_SwitchPlaces" then
-        -- TODO: Find a way to make this dynamic and not hardcoded to only switch actor[1] and actor[2]
-        -- Sex:SwitchPlaces(caster) -- bundle all of this in a new function instead to handle place switching
-        local scene = Scene:FindSceneByEntity(caster)
-        local savedActor = scene.actors[1]
+    -- if spell == "BG3SX_SwitchPlaces" then
+    --     -- TODO: Find a way to make this dynamic and not hardcoded to only switch actor[1] and actor[2]
+    --     -- Sex:SwitchPlaces(caster) -- bundle all of this in a new function instead to handle place switching
+    --     local scene = Scene:FindSceneByEntity(caster)
+    --     local savedActor = scene.actors[1]
 
-        Ext.ModEvents.BG3SX.SceneSwitchPlacesBefore:Throw({scene.actors})
-        scene.actors[1] = scene.actors[2]
-        scene.actors[2] = savedActor
-        Ext.ModEvents.BG3SX.SceneSwitchPlacesAfter:Throw({scene.actors})
+    --     Ext.ModEvents.BG3SX.SceneSwitchPlacesBefore:Throw({scene.actors})
+    --     scene.actors[1] = scene.actors[2]
+    --     scene.actors[2] = savedActor
+    --     Ext.ModEvents.BG3SX.SceneSwitchPlacesAfter:Throw({scene.actors})
 
-        scene:CancelAllSoundTimers() -- Cancel all currently saved soundTimers to not get overlapping sounds
-        Sex:PlayAnimation(caster, scene.currentAnimation)
-    end
-    if spell == "BG3SX_ChangeCameraHeight" then
-        Sex:ChangeCameraHeight(caster)
-    end
-    if spell == "BG3SX_StopAction" then
-        local scene = Scene:FindSceneByEntity(caster)
-        scene:Destroy()
-    end
+    --     scene:CancelAllSoundTimers() -- Cancel all currently saved soundTimers to not get overlapping sounds
+    --     Sex:PlayAnimation(caster, scene.currentAnimation)
+    -- end
+    -- if spell == "BG3SX_ChangeCameraHeight" then
+    --     Sex:ChangeCameraHeight(caster)
+    -- end
+    -- if spell == "BG3SX_StopAction" then
+    --     local scene = Scene:FindSceneByEntity(caster)
+    --     scene:Destroy()
+    -- end
 end)
